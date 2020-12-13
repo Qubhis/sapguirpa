5cf24d8bbb551d9202ce8ec2245d5bad32989057
Jakub <hajek.jakub@hotmail.com>
2020-12-13 17:24:33 +0100

    docstrings for most of the code

--- sap_gui_rpa/common/sapguirpa.py
+++ sap_gui_rpa/common/sapguirpa.py
@@ -1,23 +1,36 @@
 import win32com.client
 import pywintypes
 
-'''
-NOTE: You can see several functions except the class which I wrote at the very
-beginning of my days with SAP GUI scripting. Those can be omitted except 
-two of them, gui_crash_report and gui_dropdown_selection as those two are 
-used in method attach_to_session.  
-'''
 
 class SapGuiRpa:
-    '''
-    Wrapper around GuiApplication object to simplify script development.
+    """Wrapper around GuiApplication object to simplify script development.
+
     Code should be mostly self-explanatory, but in case it's not, please use
     SAP official documentation for more information:
     https://help.sap.com/viewer/b47d018c3b9b45e897faf66a6c0885a8/760.05/en-US/babdf65f4d0a4bd8b40f5ff132cb12fa.html
 
     Supports attaching to the running instance of SAP GUI through the Running
     Object Table only. This wrapper doesn't support automation of logging in.
-    '''
+
+    I highly recommend to use scripting tracker along when creating your own
+    scripts. It will help you to identify correct element's id on a screen,
+    and support recording in python code -> https://tracker.stschnell.de/ 
+
+    Attributes
+    ----------
+    sap_gui_object : win32com.clientCDispatch
+        running object entry of SAPGUI
+
+    application : win32com.clientCDispatch 
+        running SAP Logon process - GuiApplication
+
+    connection : win32com.clientCDispatch
+        connection between SAP GUI and an application server - GuiConnection
+
+    session : win32com.clientCDispatch
+        access point for applications - GuiSession
+    """
+
     def __init__(self):
         self.sap_gui_object = None
         self.application = None
@@ -26,16 +39,26 @@ class SapGuiRpa:
 
 
     def _get_available_sessions(self):
-        '''scans for all connections and their sessions of SAP Gui instance.
+        """internal method for getting a list of all available session
+        which are not in busy state
 
-        returns dictionary with session titles and indexes for connection
-        '''
+        Returns
+        -------
+        {dict}
+            key is title(str) of Session, value is dict containing child's 
+            indexes(int) for connection and session
+            {
+                title: {
+                    "conn_index": int, "sess_index": int
+                }
+            }
+        """
         available_sessions = dict()
         for conn_index, connection in enumerate(self.application.Connections):
             for sess_index, session in enumerate(connection.Sessions):
                 if session.Busy:
                     continue
-                # get title of the main window - only child of Session object
+                # get title of the main window - the only child of Session obj
                 title = session.Children(0).Text
                 session_details = {
                     title: {
@@ -49,20 +72,30 @@ class SapGuiRpa:
 
 
     def attach_to_session(self, mode="cli"):
-        ''' 
-        Lists all available, non-busy sessions and prompts for selection 
-        through command line interface by default, or through GUI window (PySimpleGui)
+        """Prompts user to select one of available sessions for attaching to
+        and updates instance attributes
 
-        Throws SapLogonNotStarted if SAP Logon is not running.
+        Parameters
+        ----------
+        mode : str, optional
+            "cli" by default - prompts via command line interface;
+            "gui" - prompts via GUI window
 
-        Updates class instance attributes.
-        '''
+        Raises
+        ------
+        SapLogonNotStarted
+            if SAP Logon instance is not running
+
+        NoAvailableSession
+            when no session is running or all of them are in busy state
+        """
+        # Get the application object from the running object table (ROT)
         try:
             self.sap_gui_object = win32com.client.GetObject("SAPGUI")
         except pywintypes.com_error as com_error:
             if com_error.args[0] == -2147221020:
                 raise SapLogonNotStarted
-
+        
         self.application = self.sap_gui_object.GetScriptingEngine
 
         available_sessions = self._get_available_sessions()
@@ -73,29 +106,63 @@ class SapGuiRpa:
         selected_session = select_session(available_sessions, mode)
         conn_index = available_sessions[selected_session]['conn_index']
         sess_index = available_sessions[selected_session]['sess_index']
-        # update attributes
+        # update remaining attributes
         self.connection = self.application.Children(conn_index)
         self.session = self.connection.Children(sess_index)
 
 
     def start_transaction(self, transaction):
+        """Executes transaction code
+
+        Parameters
+        ----------
+        transaction : str
+            transaction code
+
+        Examples
+        ----------
+        >>> SapGuiRpa.start_transaction("ME21N")
+
+        has the same effect as you would type "/nME21N" in SAP GUI's command
+        line
+        """
         self.session.StartTransaction(transaction)
 
 
     def end_transaction(self):
+        """returns to SAP Easy Access screen - same effect as you would type
+        "/n" in SAP GUI's command line
+        """
         self.session.EndTransaction()
 
 
     def gui_maximize(self):
+        """Maximazes the main window """
         self.get_element_by_id("wnd[0]").Maximize()
 
 
     def gui_restore_size(self):
+        """restores the main window and its modal windows(if exists)"""
         self.get_element_by_id("wnd[0]").Restore()
 
 
-    def send_vkey(self, vkey, window=None):
-        '''executes virtual key as per below - please add if missing
+    def send_vkey(self, vkey, window="wnd[0]"):
+        """Executes virtual key on the window
+
+        Parameters
+        ----------
+        vkey : int
+            number of the virtual keys (list below)
+        window : str, optional
+            id of the window, by default "wnd[0]" - main
+
+        Raises
+        ------
+        AssertionError
+            only if not implemented in this class
+
+        List of implemented virtual keys
+        --------------------------------
             0 -> Enter
             2 -> F2
             3 -> F3
@@ -103,12 +170,9 @@ class SapGuiRpa:
             11 -> Save
             81 -> PageUp
             82 -> PageDown
-
-        window is by default 'wnd[0]' if not provided
-            '''
-        if window is None:
-            window = "wnd[0]"
-
+        
+        More keys can be implemented if needed, full list in SAP help
+        """
         if vkey not in (0, 2, 3, 8, 11, 81, 82):
             raise AssertionError(f"Vkey {vkey} is not supported!")
         else:
@@ -116,11 +180,24 @@ class SapGuiRpa:
 
 
     def insert_value(self, element_id, value):
-        '''takes element ID path and value to be inserted
-        Inserts the value to the field. 
+        """takes element id and inserts provided value to its respective
+        attribute (text or key) based on the element's type
+
+        Supported types are GuiTextField, GuiCTextField, GuiComboBox
         
-        Returns nothing'''
-        
+        Parameters
+        ----------
+        element_id : str
+            e.g. "wnd[0]/usr/ctxtEM_MATNR-LOW"
+
+        value : str
+            value to be passed to the attribute
+
+        Raises
+        ------
+        AssertionError
+            if the type of provided element is not compatible
+        """        
         element = self.get_element_by_id(element_id)
 
         if element.type in ("GuiTextField", "GuiCTextField"):
@@ -132,17 +209,32 @@ class SapGuiRpa:
 
 
     def press_or_select(self, element_id, check=True):
-        '''takes element ID path, optionally check=False to indicate
-        desire for un-checking a checkbox.
-        Action press or select is based on .type property of the element:
+        """Executes press, select, or setFocus method based on element's type
+
+        Modifies attribute 'selected' if the element is a checkbox
+
+        supported elements:
          - GuiButton
          - GuiCheckBox
          - GuiRadioButton
          - GuiTab
          - GuiMenu
          - GuiLabel - in search results or just simple text label
- 
-        Returns nothing'''
+
+
+        Parameters
+        ----------
+        element_id : str
+            e.g. "wnd[0]/usr/ctxtEM_MATNR-LOW"
+
+        check : bool, optional
+            only for checkbox (GuiCheckBox), by default True
+
+        Raises
+        ------
+        AssertionError
+            if the type of provided element is not compatible
+        """
         element = self.get_element_by_id(element_id)
         
         if element.type == 'GuiButton':
@@ -170,39 +262,85 @@ class SapGuiRpa:
 
 
     def get_element_by_id(self, element_id):
-        ''' takes element id
-        , returns element as an object -> we can use properties and methods
-        from GuiAplication object (SAPGUI) when needed'''
+        """takes element id and returns its instance for further manipulation
+
+        Parameters
+        ----------
+        element_id : str
+            e.g. "wnd[0]/usr/ctxtEM_MATNR-LOW"
+
+        Returns
+        -------
+        {object}
+            instance of the element
+        """
         return self.session.findById(element_id)
 
 
     def get_element_text(self, element_id):
-        ''' takes element ID,
-        returns value of the element - if string value'''
+        """returns text property of an element
+
+        Parameters
+        ----------
+        element_id : str
+            e.g. "wnd[0]/usr/ctxtEM_MATNR-LOW"
+
+        Returns
+        -------
+        {str}
+            value of element's text property
+        """
         return self.get_element_by_id(element_id).text
 
 
     def get_element_type(self, element_id):
-        '''takes in element_id
-        returns type of the element'''
+        """returns element type property of an element
+
+        Parameters
+        ----------
+        element_id : str
+            e.g. "wnd[0]/usr/ctxtEM_MATNR-LOW"
+
+        Returns
+        -------
+        {str}
+            value of element's type property
+        """
         return self.get_element_by_id(element_id).type
 
 
     def get_status_bar(self):
-        '''returns status bar data in format tuple(message_type, text)
-         message types are
-            - S success
-            - W warning
-            - E error
-            - A abort
-            - I information
-        '''
+        """returns message type and text properties of status bar object
+        (GuiStatusbar)
+
+        Returns
+        -------
+        {tuple}
+            tuple containing message type and text, e.g. (message_type, text)
+        
+        message types are:
+            S = success,
+            W = warning,
+            E = error,
+            A = abort,
+            I = information
+        """
         status_bar = self.get_element_by_id("wnd[0]/sbar")
         return (status_bar.MessageType, status_bar.text)
 
 
     def verify_element(self, element_id):
-        '''returns true if element is found on a screen'''
+        """returns True if element is found on a screen, else False
+
+        Parameters
+        ----------
+        element_id : str
+            e.g. "wnd[0]/usr/ctxtEM_MATNR-LOW"
+
+        Returns
+        -------
+        {bool}
+        """
         try:
             self.get_element_by_id(element_id)
             return True
@@ -264,6 +402,8 @@ class SapGuiRpa:
 
 
     def disconnect(self):
+        """to be used in the end of your script to stop active scripting
+        """
         self.session = None
         self.connection = None
         self.application = None
@@ -298,15 +438,12 @@ def select_session(available_sessions, mode="cli"):
     return session_title
 
 
-def gui_dropdown_selection(title='Select one option', dropdown_list=None):
+def gui_dropdown_selection(title, dropdown_list):
     '''takes title of the gui and list of items for selection
     creates gui window with dropdown selection
-    returns value of selected item or None if closed'''
+    returns value of selected item or None if closed'''  
     import PySimpleGUI as sg
     
-    if dropdown_list is None:
-        dropdown_list = list()
-    assert len(dropdown_list) > 0, "provided list is empty!"
     layout = [
         [sg.Text('Please choose one of below options:')],
         [sg.InputCombo(dropdown_list, size=(40, 10))],
